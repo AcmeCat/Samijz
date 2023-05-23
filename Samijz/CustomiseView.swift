1882c8eb5a16d0e5da5835cce7046a1ee8b04a43
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-05-23 21:42:18 +0800

    modified lots of images. added new sandwich options for sheeze and bread

--- Samijz/CustomiseView.swift
+++ Samijz/CustomiseView.swift
@@ -11,33 +11,51 @@ struct CustomiseView: View {
     let sandwich: Sandwich
     
     @State private var bread = 0
-    @State private var addDrink = false
+    @State private var isToasted = true
+    @State private var extraSheeze = false
+    @State private var hasCrust = true
+    @State private var cuts = 0
+    @State private var sheeze = 0
     
     let breadOptions = ["White", "Whole", "Grainy", "Gluten-free"]
+    
+    let cutOptions = ["Uncut", "2 Triangles", "4 Triangles", "2 Rectangles", "3 Rectangles", "4 Squares"]
+    
+    let sheezeOptions = ["None (Heretic)", "Sharp", "Tasty", "Melty", "House Mix"]
     
     var calories: Int {
         100
     }
     
-    var protein: Int {
-        15
-    }
-    
     var body: some View {
         Form {
-            Section("Basic options") {
+            Section("Bread Options") {
                 Picker("Bread", selection: $bread) {
-                    ForEach(breadOptions.indices) { index in
+                    ForEach(breadOptions.indices, id: \.self) { index in
                         Text(breadOptions[index])
                     }
                 }
                 .pickerStyle(.segmented)
-                
-                Toggle("Add Drink", isOn: $addDrink)
+                Toggle("Toasted", isOn: $isToasted)
+                Picker("Cuts", selection: $cuts) {
+                    ForEach(cutOptions.indices, id: \.self) { index in
+                        Text(cutOptions[index])
+                    }
+                }
+                .pickerStyle(.segmented)
+                Toggle("Crusts", isOn: $hasCrust)
+            }
+            Section("Sheeze Options") {
+                Picker("Sheeze", selection: $sheeze) {
+                    ForEach(sheezeOptions.indices, id: \.self) { index in
+                        Text(sheezeOptions[index])
+                    }
+                }
+                .pickerStyle(.segmented)
+                Toggle("Extra Sheeze", isOn: $extraSheeze)
             }
             Section ("Estimates") {
                 Text("**Calories:** \(calories) kcal")
-                Text("**Protein:** \(protein) grams")
             }
         }
         .navigationBarTitleDisplayMode(.inline)
