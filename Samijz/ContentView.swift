bb5f437ec6220104291346bd3e40bc9e01a83379
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-05-27 16:47:47 +0800

    modified content view to display list of servings (your current order)

--- Samijz/ContentView.swift
+++ Samijz/ContentView.swift
@@ -10,10 +10,45 @@ import CoreData
 
 struct ContentView: View {
     @StateObject var menu = Menu()
+    @StateObject var order = Order()
+    @State private var showingAddScreen = false
     
     var body: some View {
-        MenuView()
-            .environmentObject(Menu())
+        NavigationView {
+            List {
+                if order.servings.isEmpty {
+                    Button("Add your first item") {
+                        showingAddScreen = true
+                    }
+                } else {
+                    ForEach(order.servings) {serving in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(serving.name)
+                                    .font(.headline)
+                                Text(serving.description)
+                                    .font(.caption)
+                            }
+                            
+                            Spacer()
+                            
+                            Text("\(serving.calories) kCal")
+                        }
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddScreen, content: MenuView.init)
+            .navigationTitle("Samijz")
+            .toolbar {
+                Button {
+                    showingAddScreen = true
+                } label: {
+                    Label("Add New Item", systemImage: "plus")
+                }
+            }
+        }
+        .environmentObject(menu)
+        .environmentObject(order)
     }
 }
 
