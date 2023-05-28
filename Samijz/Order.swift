6b611001dbf6057a4a74152272553b2f938d111b
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-05-28 15:29:18 +0800

    bugfix: modified natural language algorithm

--- Samijz/Order.swift
+++ Samijz/Order.swift
@@ -33,31 +33,28 @@ class Order: ObservableObject {
         servings.reduce(0) { $0 + $1.cost }
     }
     
-    //revise this after you modify the data model
+    //TODO: revise this after you modify the data model
     func add(_ item: Item, bread: String, cut: String, toasted: Bool, crusts: Bool, sheeze: String, extraSheeze: Bool, sauce: CustomOption,
              shoog: CustomOption, salt: CustomOption, pie: CustomOption, cookie: CustomOption, cooler: Bool, calories: Int, cost: Int) {
         var description = [String]()
         if item.sangaOptions {
-            description.append("\(cut == "Uncut " ? cut : "")\(toasted ? "Toasted " : "") \(item.name) Samij")
-            description.append("on \(bread) bread")
-            if (sheeze != "none (heretic)") { description.append("with \(extraSheeze ? "extra " : "")\(sheeze) sheeze")}
+            description.append("\(cut != "Uncut" ? !toasted ? "O" : "Toasted, o" : !toasted ? "Uncut, o" : "Toasted, uncut, o")n \(bread.lowercased()) bread")
+            if (sheeze != "None (Heretic)") { description.append("with \(extraSheeze ? "extra " : "")\(sheeze.lowercased()) sheeze")}
             if (shoog != CustomOption.none) { description.append("with \(shoog.name) shoog")}
-            description.append("and \(sauce == CustomOption.none ? "no" : sauce.name) sauce")
-            if (cut != "Uncut") {description.append("cut into \(cut)")}
+            description.append("and \(sauce == CustomOption.none ? "no" : sauce.name.lowercased()) sauce")
+            if (cut != "Uncut") {description.append("cut into \(cut.lowercased())")}
             if !crusts { description.append("and no crusts") }
         } else if item.drinkOptions {
-            description.append(item.name)
-            if cooler { description.append(" with party clothes") }
+            if cooler { description.append("With party clothes included") }
         } else if item.saltOptions {
-            description.append(item.name)
-            if (salt != CustomOption.none) { description.append("with \(salt.name) salt")}
-            description.append("and \(sauce == CustomOption.none ? "no" : sauce.name) sauce")
+            if (salt != CustomOption.none) { description.append("With \(salt.name.lowercased()) salt")}
+            description.append("and \(sauce == CustomOption.none ? "no" : sauce.name.lowercased()) sauce")
         } else if item.pieOptions {
-            description.append("\(pie.name) \(item.name)")
-            if (shoog != CustomOption.none) { description.append("with \(shoog.name) shoog")}
+            description.append("\(pie.name.prefix(1).capitalized + pie.name.dropFirst().lowercased()) \(item.name.lowercased())")
+            if (shoog != CustomOption.none) { description.append("with \(shoog.name.lowercased()) shoog")}
         } else if item.cookieOptions {
-            description.append("\(cookie.name) \(item.name)")
-            if (shoog != CustomOption.none) { description.append("with \(shoog.name) shoog")}
+            description.append("\(cookie.name.prefix(1).capitalized + cookie.name.dropFirst().lowercased()) \(item.name.lowercased())")
+            if (shoog != CustomOption.none) { description.append("with \(shoog.name.lowercased()) shoog")}
         }
         let descriptionString = description.joined(separator: ", ")
         
