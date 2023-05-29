df4c7b789c07f330132ce583d3c1fc516b66ccda
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-05-29 21:12:37 +0800

    Added Constants struct to hold literal values for UIComponents

---
+++ Samijz/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Samijz
+//
+//  Created by Rory Allen on 29/5/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    struct Colors {
+        static let primary = Color(red: 107/255, green: 92/255, blue: 165/255)
+        static let secondary = Color(red: 114/255, green: 25/255, blue: 90/255)
+        static let tertiary = Color(red: 198/255, green: 216/255, blue: 255/255)
+    }
+}
